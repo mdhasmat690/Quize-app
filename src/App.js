ab02d8582d83e9c2e29df9e681e3c1afcc6776f1
Md- Hasmat <mdhasmat690>
2022-09-20 11:48:27 +0600

    firebase added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "../src/styles/App.css";
 import Layout from "./components/Layout";
 import Home from "./components/pages/Home";
 import Login from "./components/pages/Login";
+import Quiz from "./components/pages/Quize";
+import Result from "./components/pages/Result";
 import Signup from "./components/pages/Signup";
+import { AuthProvider } from "./context/AuthContext";
 function App() {
   return (
-    <Layout>
-      <Login />
-      <br />
-      <br />
-      <br />
-      <br />
+    <Router>
+      <AuthProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/login" component={Login} />
 
-      <Signup />
-      <Home />
-    </Layout>
+            <Route exact path="/quiz" component={Quiz} />
+            <Route exact path="/result" component={Result} />
+          </Switch>
+        </Layout>
+      </AuthProvider>
+    </Router>
   );
 }
 
